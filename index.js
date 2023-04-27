184b7f9698b0ba3595844efe5589954eb18d3216
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-04-27 11:49:57 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(cat) {
+    const copyCats = cats.slice();
+    copyCats.push(cat);
+    return copyCats;
+}
+
+function prependCat(cat) {
+    const copyCats = cats.slice();
+    copyCats.unshift(cat);
+    return copyCats;
+}
+
+function removeLastCat() {
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats
+}
+
+const cats = ["Milo", "Otis", "Garfield"];
